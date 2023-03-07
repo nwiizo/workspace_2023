fcf586aac92a1e131c942c62c16920c0c176d374
nwiizo <syu.m.5151@gmail.com>
2023-03-07 13:21:11 +0900

    🐶

--- go-git/main.go
+++ go-git/main.go
@@ -5,8 +5,35 @@ import (
 	"os"
 	"strings"
 
-	"github.com/go-git/go-git/v5"
+	git "github.com/go-git/go-git/v5"
 )
+
+
+// Basic example of how to clone a repository using clone options.
+func main() {
+	CheckArgs("<url>", "<directory>")
+	url := os.Args[1]
+	directory := os.Args[2]
+
+	// Clone the given repository to the given directory
+	Info("git clone %s %s --recursive", url, directory)
+
+	r, err := git.PlainClone(directory, false, &git.CloneOptions{
+		URL:               url,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	})
+
+	CheckIfError(err)
+
+	// ... retrieving the branch being pointed by HEAD
+	ref, err := r.Head()
+	CheckIfError(err)
+	// ... retrieving the commit object
+	commit, err := r.CommitObject(ref.Hash())
+	CheckIfError(err)
+
+	fmt.Println(commit)
+}
 
 // CheckArgs should be used to ensure the right command line arguments are
 // passed before executing an example.
@@ -35,30 +62,4 @@ func Info(format string, args ...interface{}) {
 // Warning should be used to display a warning
 func Warning(format string, args ...interface{}) {
 	fmt.Printf("\x1b[36;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
-}
-
-// Basic example of how to clone a repository using clone options.
-func main() {
-	CheckArgs("<url>", "<directory>")
-	url := os.Args[1]
-	directory := os.Args[2]
-
-	// Clone the given repository to the given directory
-	Info("git clone %s %s --recursive", url, directory)
-
-	r, err := git.PlainClone(directory, false, &git.CloneOptions{
-		URL:               url,
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-	})
-
-	CheckIfError(err)
-
-	// ... retrieving the branch being pointed by HEAD
-	ref, err := r.Head()
-	CheckIfError(err)
-	// ... retrieving the commit object
-	commit, err := r.CommitObject(ref.Hash())
-	CheckIfError(err)
-
-	fmt.Println(commit)
 }
