58128267cd7cae321e52104da4b93e569e9bab4f
nwiizo <syu.m.5151@gmail.com>
2023-04-19 17:05:28 +0900

    🐾 [WIP]

---
+++ openai-go/openai/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	client := openai.NewClient("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")
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
